43e4d79415b0f1ac4eb02ec828f9c3239806c45b
Okware-L <okware@DESKTOP-996KE1O>
2023-01-02 01:31:08 +0300

    voting

---
+++ scripts/9-setup-vote.js
@@ -0,0 +1,56 @@
+import sdk from "./1-initialize-sdk.js";
+
+(async () => {
+  try {
+    // This is our governance contract.
+    const vote = await sdk.getContract(
+      "0x01C5C704C3438EdaB33c416715Da84D39F85d457",
+      "vote"
+    );
+    // This is our ERC-20 contract.
+    const token = await sdk.getContract(
+      "0x273dE5cb9c0A0B4dC479487BB79Bdc57808680d9",
+      "token"
+    );
+    // Give our treasury the power to mint additional token if needed.
+    await token.roles.grant("minter", vote.getAddress());
+
+    console.log(
+      "Successfully gave vote contract permissions to act on token contract"
+    );
+  } catch (error) {
+    console.error(
+      "failed to grant vote contract permissions on token contract",
+      error
+    );
+    process.exit(1);
+  }
+
+  try {
+    // This is our governance contract.
+    const vote = await sdk.getContract(
+      "0x01C5C704C3438EdaB33c416715Da84D39F85d457",
+      "vote"
+    );
+    // This is our ERC-20 contract.
+    const token = await sdk.getContract(
+      "0x273dE5cb9c0A0B4dC479487BB79Bdc57808680d9",
+      "token"
+    );
+    // Grab our wallet's token balance, remember -- we hold basically the entire supply right now!
+    const ownedTokenBalance = await token.balanceOf(process.env.WALLET_ADDRESS);
+
+    // Grab 90% of the supply that we hold.
+    const ownedAmount = ownedTokenBalance.displayValue;
+    const percent90 = (Number(ownedAmount) / 100) * 90;
+
+    // Transfer 90% of the supply to our voting contract.
+    await token.transfer(vote.getAddress(), percent90);
+
+    console.log(
+      "Successfully transferred " + percent90 + " tokens to vote contract"
+    );
+  } catch (err) {
+    console.error("failed to transfer tokens to vote contract", err);
+  }
+})();
